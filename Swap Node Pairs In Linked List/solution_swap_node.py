ec59696ce315bfa49610caa35873425374e97869
FIRST_NAME LAST_NAME <dzoban.pn@ucu.edu.com>
2024-04-08 21:27:03 +0300

    solved Swap Node Pairs In Linked List

---
+++ Swap Node Pairs In Linked List/solution_swap_node.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, next=None):
+        self.next = next
+def swap_pairs(head):
+    if head is None or (head.next is None):
+        return head
+    dummy=Node(0)
+    dummy.next = head
+    prev = dummy
+    while prev.next and prev.next.next:
+        first = prev.next
+        second = prev.next.next
+        prev.next = second
+        first.next = second.next
+        second.next = first
+        prev = first
+    return dummy.next
